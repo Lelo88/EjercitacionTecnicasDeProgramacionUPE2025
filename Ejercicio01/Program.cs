8133b0731c6261030d582efb97f9ccb7ef134f33
Leandro Gabriel Villalba <villalbaleandrogabriel@gmail.com>
2025-03-24 19:13:34 -0300

    Base primer ejercicio

---
+++ Ejercicio01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[4];
+            int suma = 0;
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write("Introduce un número: ");
+                numeros[i] = Convert.ToInt32(Console.ReadLine());
+                suma += numeros[i];
+            }
+
+            double promedio = (double)suma / numeros.Length;
+            Console.WriteLine("El promedio de los números es: " + promedio);
+        }
+    }
+}
